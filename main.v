083e2e1127e7c70adea60e23a839c0cde88dd625
Delta456 <swstkbaranwal@gmail.com>
2020-07-05 14:24:02 +0530

    rewrite

---
+++ main.v
@@ -0,0 +1,6 @@
+import repl
+
+fn main() {
+	println("Hello, this is the Monkey programming language implementation in V!")
+	repl.start()
+}
---
+++ repl/repl.v
@@ -0,0 +1,27 @@
+module repl
+
+import os
+import lexer
+import token
+
+
+const (
+	prompt = ">> "
+)
+
+pub fn start() {
+	for {
+		print(prompt)
+		line := os.get_line()
+		if line == 'exit' {
+			 break 
+		}
+		mut l := lexer.new(line)
+		mut tok := l.next_token()
+
+		for tok.typ != token.eof {
+			println("token ${tok.typ} : ${tok.literal}")
+			tok = l.next_token()
+		}
+	}
+}
